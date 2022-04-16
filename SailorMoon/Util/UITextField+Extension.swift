3f2c280313e98461f5ef289dde7393b94f92b86a
longdengyu <464050079@qq.com>
2022-04-16 22:43:09 +0800

    Update: 内容页布局

---
+++ SailorMoon/Util/UITextField+Extension.swift
@@ -0,0 +1,33 @@
+//
+//  Extension.swift
+//  SailorMoon
+//
+//  Created by LongDengYu on 2022/4/16.
+//
+
+import UIKit
+
+class TextField: UITextField {
+    var insetX: CGFloat = 6 {
+       didSet {
+         layoutIfNeeded()
+       }
+    }
+    var insetY: CGFloat = 6 {
+       didSet {
+         layoutIfNeeded()
+       }
+    }
+
+    // placeholder position
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: insetX, dy: insetY)
+    }
+
+    // text position
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: insetX, dy: insetY)
+    }
+    
+}
+
