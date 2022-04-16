240c1a1bc37b4b2a421b7f4c435adfb8d3606f1f
suyixiao <1580925976@qq.com>
2022-04-16 14:51:37 +0800

    [Fix]主controller定向问题

--- SailorMoon/MainPage/MainPageTableViewController.swift
+++ SailorMoon/MainPage/MainPageTableViewController.swift
@@ -12,6 +12,7 @@ class MainPageTableViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .brown
     }
 
 
