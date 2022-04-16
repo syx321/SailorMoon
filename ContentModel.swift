3e445ad73f644a4b833bcf6d9339f5045e4572e9
longdengyu <464050079@qq.com>
2022-04-16 20:33:19 +0800

    内容页布局

---
+++ ContentModel.swift
@@ -0,0 +1,18 @@
+//
+//  ContentModel.swift
+//  SailorMoon
+//
+//  Created by LongDengYu on 2022/4/16.
+//
+
+import UIKit
+
+struct ContentModel {
+    var title: String?
+    var content: String?
+    var images: [UIImage]?
+    var updateAt: Date?
+    var buildTime: Date?
+    var coverImage: UIImage?
+    
+}
